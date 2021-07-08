a159a8c016f186897a7e6c64e847a521b2a746ef
unknown <fisnikmaliqi.fm@gmail.com>
2021-07-08 02:41:46 +0200

    working on laravel project

---
+++ resources/views/pages/results.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+    
+    <div class="jumbotron text-center">
+
+        <table class="table">
+            
+            <h1>Tabela perfundimtare e superliges Kosovare per sezonin 2020/21</h1>
+            <thead>
+              <tr>
+                <th scope="col">Ekipi vendas</th>
+                <th scope="col">Rezultati</th>
+                
+                <th scope="col">Ekipi mysafir</th>
+                <th scope="col">Data</th>
+                <th scope="col">Ora</th>
+                
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="bg-success">
+                <th scope="row">1</th>
+                <td> <b>Prishtina</b></td>
+                <td>15</td>
+                <td>6</td>
+                <td>64</td>
+              </tr>
+             
+            </tbody>
+
+           
+          </table>
+    
+
+    
+    </div>
+ @endsection
+
